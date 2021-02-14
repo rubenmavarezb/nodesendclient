9be6038c865055815e26cb25cf52ca1794316355
Ruben Mavarez <rmavarez@glamit.com.ar>
2021-02-14 11:49:33 -0300

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
         backendURL: 'https://guarded-sierra-56199.herokuapp.com/',
-        frontendURL: 'https://nodesendclient-nine.vercel.app'
+        frontendURL: 'https://nodesendclient-vert.vercel.app/'
         // backendURL: 'http://localhost:4000',
         // frontendURL: 'http://localhost:3000/'
     }
